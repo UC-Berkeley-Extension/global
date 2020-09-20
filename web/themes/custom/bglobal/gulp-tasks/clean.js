131e64281e36d48f70c931cde7265850fee684dc
Ju <ju.vanderw@gmail.com>
2020-09-19 22:22:12 -0500

    Setting up gulp

---
+++ web/themes/custom/bglobal/gulp-tasks/clean.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Include Our Plugins
+const del = require('del');
+
+// Export our tasks.
+module.exports = {
+  // Clean CSS files.
+  cleanCSS: function() {
+    return del(['./dist/css/*'], { force: true });
+  },
+
+  // Clean JS files.
+  cleanJS: function() {
+    return del(['./dist/js/*'], { force: true });
+  },
+
+  // Clean image files.
+  cleanImages: function() {
+    return del(['./dist/images/*'], { force: true });
+  },
+
+  // Clean font files.
+  cleanFonts: function() {
+    return del(['./dist/fonts/*'], { force: true });
+  },
+};
