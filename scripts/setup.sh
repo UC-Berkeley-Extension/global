7246dc79423b5ca49fa9841d928f9854fdf711e2
agentrickard <agentrickard@gmail.com>
2020-09-22 13:50:26 -0400

    Removes bad includes from theme.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # be in the right directory
 cd /var/www/html
+echo 'Removing old development settings...'
+rm web/sites/default/development.services.yml
 echo 'Copying settings file...'
 cp config/settings/settings.ddev.php web/sites/default
 echo 'Installing site...'
 cd web
 drush si --existing-config
+echo 'Site installed...'
+echo 'Copying development settings file...'
+cp ../config/settings/development.services.yml sites/default
 echo 'Done!'
+drush cr
