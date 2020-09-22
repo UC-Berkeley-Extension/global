a3e627d706067e21f21327088cfa4d81c166a8ca
agentrickard <agentrickard@gmail.com>
2020-09-22 14:01:58 -0400

    Updates install script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # be in the right directory
 cd /var/www/html
-echo 'Removing old development settings...'
 echo 'Copying settings file...'
 cp config/settings/settings.ddev.php web/sites/default
 echo 'Installing site...'
