0b04c9d0f5f037e0c50c41df244603f54f999e85
agentrickard <agentrickard@gmail.com>
2020-09-24 15:00:33 -0400

    On refresh, copy settings files.

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # be in the right directory
 cd /var/www/html/web
+echo 'Copying settings file...'
+cp ../config/settings/settings.ddev.php sites/default
+echo 'Copying development settings file...'
+cp ../config/settings/development.services.yml sites/default
 echo 'Clearing cache...'
 drush cr
 echo 'Importing configuration...'
