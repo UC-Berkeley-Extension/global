1d7bd098073fc600d5218e0488c5eb563663c342
agentrickard <agentrickard@gmail.com>
2020-09-29 16:58:30 -0400

    Updates JSON block for Voices posts.

--- web/modules/custom/spocc_angular/components/voices_list/component.ts
+++ web/modules/custom/spocc_angular/components/voices_list/component.ts
@@ -12,12 +12,17 @@ import 'rxjs/add/operator/map';
 export class VoicesList{
   url: string = 'https://voices.berkeley.edu/international-feed/post/all/tag/all/program/all';
 
+  // Constructor method sets our data from the JSON callback.
   constructor(@Inject(Http) http: Http) {
+    // TODO: error handling.
     http.get(this.url).map(res => res.json()).subscribe((data) {
-      this.voices = data.nodes;
-      console.log(this.voices);
+      var items = data.nodes;
+      // Data is nested inside the 'post' of each item.
+      this.posts = Object.keys(items).map(key => items[key].post);
     });
   }
+
+  // Helper function to check data structures.
   getKeys(obj){
     return Object.keys(obj)
   }
