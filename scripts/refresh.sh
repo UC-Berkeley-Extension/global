801b154e83e0c404e9e7fe6a8638b69e5180ea60
agentrickard <agentrickard@gmail.com>
2020-10-01 15:39:43 -0400

    Include default content in the setup process.

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # be in the right directory
 cd /var/www/html/web
+echo 'Copying default files...'
+mkdir sites/default/files/2020-10
+cp modules/custom/bglobal_test_content/assets/*.* sites/default/files/2020-10
 echo 'Clearing cache...'
 drush cr
 echo 'Importing configuration...'
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,8 @@ echo 'Copying settings file...'
 cp config/settings/settings.ddev.php web/sites/default
 echo 'Installing site...'
 cd web
+mkdir sites/default/files/2020-10
+cp modules/custom/bglobal_test_content/assets/*.* sites/default/files/2020-10
 drush si --existing-config
 echo 'Site installed...'
 echo 'Copying development settings file...'
