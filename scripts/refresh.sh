5b6f363ec59e52969c17643e2671557cf409a3e3
agentrickard <agentrickard@gmail.com>
2020-10-01 15:52:29 -0400

    Remove redundancies -- default_content handles files now

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -2,8 +2,6 @@
 # be in the right directory
 cd /var/www/html/web
 echo 'Copying default files...'
-mkdir sites/default/files/2020-10
-cp modules/custom/bglobal_test_content/assets/*.* sites/default/files/2020-10
 echo 'Clearing cache...'
 drush cr
 echo 'Importing configuration...'
