b9c4943dcbab6ea837b70c3d42ddfaba08b9c945
agentrickard <agentrickard@gmail.com>
2020-10-08 16:54:03 -0400

    Set pardot block options via form.

--- web/modules/custom/bglobal_pardot/src/Plugin/Block/PardotFormBlock.php
+++ web/modules/custom/bglobal_pardot/src/Plugin/Block/PardotFormBlock.php
@@ -32,25 +32,59 @@ class PardotFormBlock extends BlockBase {
     $form['urls'] = [
       '#type' => 'details',
       '#title' => $this->t('Form URLs'),
-      '#open' => TRUE,
+      '#open' => FALSE,
     ];
     $form['urls']['action_url'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Form Action URL'),
-      '#default_value' => $this->getDefaultValue('action_url'),
+      '#default_value' => $this->configuration['action_url'],
       '#description' => $this->t('The Pardot form URL'),
     ];
     $form['urls']['success_location'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Success Location URL'),
-      '#default_value' => $this->getDefaultValue('success_location'),
+      '#default_value' => $this->configuration['success_location'],
       '#description' => $this->t('The URL to redirect to after form completion.'),
     ];
     $form['urls']['error_location'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Error Location URL'),
-      '#default_value' => $this->getDefaultValue('error_location'),
+      '#default_value' => $this->configuration['error_location'],
       '#description' => $this->t('The URL to redirect to on form failure.'),
+    ];
+    $form['fields'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Pardot Fields'),
+      '#open' => TRUE,
+    ];
+    $form['fields']['field_list'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Included fields'),
+      '#default_value' => $this->configuration['field_list'],
+      '#options' => $this->getFieldOptions(),
+    ];
+    $form['fields']['tracking_fields'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Hidden tracking fields'),
+      '#default_value' => $this->configuration['tracking_fields'],
+      '#options' => $this->getTrackingOptions(),
+    ];
+    $form['interface'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Interface options'),
+      '#open' => TRUE,
+    ];
+    $form['interface']['submit_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Submit button label'),
+      '#default_value' => $this->configuration['submit_label'],
+      '#description' => $this->t('The label for the submit button.'),
+    ];
+    $form['interface']['success_message'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Success message'),
+      '#default_value' => $this->configuration['success_message'],
+      '#description' => $this->t('Message to display after form submission.'),
     ];
     return $form;
   }
@@ -62,6 +96,10 @@ class PardotFormBlock extends BlockBase {
     $this->configuration['action_url'] = $form_state->getValue(['urls', 'action_url']);
     $this->configuration['success_location'] = $form_state->getValue(['urls', 'success_location']);
     $this->configuration['error_location'] = $form_state->getValue(['urls', 'error_location']);
+    $this->configuration['field_list'] = $form_state->getValue(['fields', 'field_list']);
+    $this->configuration['tracking_fields'] = $form_state->getValue(['fields', 'tracking_fields']);
+    $this->configuration['submit_label'] = $form_state->getValue(['interface', 'submit_label']);
+    $this->configuration['success_message'] = $form_state->getValue(['interface', 'success_message']);
   }
 
   /**
@@ -72,25 +110,77 @@ class PardotFormBlock extends BlockBase {
       'action_url' => 'https://go.pardot.com/l/102272/2016-11-04/2klckm',
       'success_location' => 'https://extension.berkeley.edu/international/',
       'error_location' => 'https://extension.berkeley.edu/international/',
-      'field_list' => [],
+      'field_list' => [
+        'firstname' => 'firstname',
+        'lastname' => 'lastname',
+        'email' => 'email',
+        'question' => 'question',
+      ],
       'submit_label' => $this->t('Submit'),
-      'tracking_fields' => [],
+      'success_message' => $this->t('Success message...'),
+      'tracking_fields' => [
+        'full_path' => 'full_path',
+        'page_title' => 'page_title',
+      ],
     ];
   }
 
   /**
-   * Returns the default value for a field,
-   *
-   * @param $field
-   *   The configuration field name.
-   *
-   * @return mixed
-   *   The value of the field as configured, or its default value.
+   * Returns the list of available fields.
    */
-  public function getDefaultValue($field) {
-    $defaults = $this->defaultConfiguration();
-    $value = $this->configuration[$field] ?: $defaults[$field];
-    return $value;
+  public function getFieldOptions() {
+    return [
+      'firstname' => 'firstname',
+      'lastname' => 'lastname',
+      'email' => 'email',
+      'question' => 'question',
+      'opt_in_contact' => 'opt_in_contact',
+    ];
+  }
+
+  /**
+   * Returns the list of available trackers.
+   */
+  public function getTrackingOptions() {
+    return [
+      'full_path' => 'full_path',
+      'page_title' => 'page_title',
+      'program_name' => 'program_name',
+    ];
+  }
+
+  public function defaultFieldSettings() {
+    $field_settings = [
+      'firstname' => [
+        'label' => 'First Name',
+        'type' => 'text',
+        'required' => TRUE,
+      ],
+      'lastname' => [
+        'label' => 'Last Name',
+        'type' => 'text',
+        'required' => TRUE,
+      ],
+      'email' => [
+        'label' => 'Email',
+        'type' => 'text',
+        'required' => TRUE,
+      ],
+      'question' => [
+        'label' => 'Question',
+        'type' => 'textarea',
+        'rows' => '5',
+        'cols' => '20',
+        'required' => FALSE,
+      ],
+      'opt_in_contact' => [
+        'label' => 'Contact List',
+        'type' => 'checkbox',
+        'description' => 'Join our mailing list.',
+        'required' => FALSE,
+      ],
+    ];
+    return $field_settings;
   }
 
   /**
@@ -99,59 +189,25 @@ class PardotFormBlock extends BlockBase {
   public function build() {
     $build = [
       '#theme' => 'bglobal_pardot_form',
-      '#action_url' => 'https://go.pardot.com/l/102272/2016-11-04/2klckm',
-      '#success_message' => 'Success!',
-      '#submit_label' => 'Act Now!',
-      '#fields' => [
-        'firstname' => [
-          'label' => 'First Name',
-          'type' => 'text',
-          'required' => TRUE,
-        ],
-        'lastname' => [
-          'label' => 'Last Name',
-          'type' => 'text',
-          'required' => TRUE,
-        ],
-        'email' => [
-          'label' => 'Email',
-          'type' => 'text',
-          'required' => TRUE,
-        ],
-        'status' => [
-          'label' => 'Status',
-          'type' => 'select',
-          'options' => ['Student', 'Professional'],
-          'default_value' => 'Professional',
-        ],
-        'question' => [
-          'label' => 'Question',
-          'type' => 'textarea',
-          'rows' => '5',
-          'cols' => '20',
-          'required' => FALSE,
-        ],
-        'opt_in_contact' => [
-          'label' => 'Contact List',
-          'type' => 'checkbox',
-          'description' => 'Join our mailing list.',
-          'required' => FALSE,
-        ],
-        'success_location' => [
-          'type' => 'hidden',
-          'default_value' => 'https://extension.berkeley.edu/international/',
-        ],
-        'error_location' => [
-          'type' => 'hidden',
-          'default_value' => 'https://extension.berkeley.edu/international/',
-        ],
-      ],
-      '#tracking' => [
-        'page_title',
-        #'program_name',
-        'full_path',
-      ],
+      '#action_url' => $this->configuration['action_url'],
+      '#success_location' => $this->configuration['success_location'],
+      '#error_location' => $this->configuration['error_location'],
+      '#success_message' => $this->configuration['success_message'],
+      '#submit_label' => $this->configuration['submit_label'],
     ];
+    $fields = $this->configuration['field_list'];
+    $field_settings = $this->defaultFieldSettings();
+    foreach ($field_settings as $key => $value) {
+      if (!empty($fields[$key])) {
+        $build['#fields'][$key] = $value;
+      }
+    }
+    $tracking = $this->configuration['tracking_fields'];
+    foreach ($tracking as $key => $value) {
+      if (!empty($value)) {
+        $build['#tracking'][] = $key;
+      }
+    }
     $build['#attached']['library'][] = 'bglobal_pardot/drupal.bglobal_pardot';
 
     return $build;
