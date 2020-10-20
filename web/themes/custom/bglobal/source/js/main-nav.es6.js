1db3df313cfb35132d0c4e9b90fecb467a2c025e
Ju <ju.vanderw@gmail.com>
2020-10-19 23:01:41 -0500

    Nav work

---
+++ web/themes/custom/bglobal/source/js/main-nav.es6.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+(function () {
+  const parent = document.querySelectorAll('.menu-depth-0');
+
+  Array.prototype.forEach.call(parent, a => {
+    const button = a.querySelector('.menu__toggle');
+    const drawer = button.nextElementSibling;
+    button.onclick = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      const expanded = button.getAttribute('aria-expanded') === 'true';
+      button.setAttribute('aria-expanded', !expanded);
+      drawer.classList.toggle('expanded');
+
+      if (!expanded) {
+        drawer.style.display = "block";
+      } else {
+        drawer.style.display = "none";
+      }
+    };
+    button.focusout = (e) => {
+      button.setAttribute('aria-expanded', false);
+      drawer.style.display = "none";
+    }
+  });
+})();
+/* eslint-enable */
