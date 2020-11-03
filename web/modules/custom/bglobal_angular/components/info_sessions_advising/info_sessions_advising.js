d7a5109c77eb482b889e71d4375073fe2ad2e578
Tommaso Boggia <tboggia@gmail.com>
2020-11-03 11:18:28 -0800

    Switch Advising from PDB to Component

---
+++ web/modules/custom/bglobal_angular/components/info_sessions_advising/info_sessions_advising.js
@@ -0,0 +1,23 @@
+// console.log("hello voices-advising");
+
+/* *
+ * Function can be plugged into angular controllers to call and manage voices feeds
+ * @return {Object} [results from Voices factory calls]
+ */
+app.controller('VoicesInfoAdvisingController', ['$scope', '$element', 'VoicesFactory', ($scope, $element, VoicesFactory) => {
+  const vcc = {};
+  vcc.attrs = document.querySelector("[data-info-sessions-advising]").dataset;
+  vcc.attrs.feed = "info";
+  VoicesFactory.getBlogs(vcc.attrs)
+    .then((data) => {
+      vcc.sessions = data.sessions.splice(1);
+      vcc.view = data.view;
+    })
+    .catch((err) => {
+      console.error('The error in VoicesInfoAdvisingController is:');
+      console.error(err);
+    });
+
+  return vcc;
+}]);
+
