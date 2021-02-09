e9997c285ecc1948dfcd58926ea81d32e3b61406
Tommaso Boggia <tboggia@gmail.com>
2021-02-08 18:06:50 -0800

    Add Program Controller to program pages

--- web/modules/custom/bglobal_angular/components/programs/programFactoryAndController.js
+++ web/modules/custom/bglobal_angular/components/programs/programFactoryAndController.js
@@ -1,15 +1,23 @@
-
+function removePoundFromKeys = (obj) => {
+  const newObj = {};
+  Object.entries(obj).forEach((pair) => {
+    let key = pair[0];
+    let value = pair[1]
+      if (key[0] == '#') key = key.substr(1, key.length-1);
+      newObj[key] = value;
+  });
+  return newObj;
+}
 app.factory('programFactory', ['$http', ($http) => {
   const pf = {};
-  pf.program = drupalSettings.program;
-  console.log(pc.program);
+  pf.program = removePoundFromKeys(drupalSettings.program);
   return pf;
 }]);
 
-app.controller('ProgramController',  ['$scope', 'productsFactory', ($scope, productsFactory) => {
+app.controller('ProgramController',  ['$scope', 'programFactory', ($scope, programFactory) => {
   const pc = {};
-  pc.program = pf.program;
-  console.log(pc.program);
+  const pf = programFactory;
+  $scope.program = pf.program;
+  console.log($scope.program);
   return pc;
 }]);
-
