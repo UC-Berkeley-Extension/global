0f6b0078152fe86307398dd28f9bfdac133c505d
Tommaso Boggia <tboggia@gmail.com>
2021-02-17 22:56:48 -0800

    Set up courses

--- web/modules/custom/bglobal_angular/components/programs/programFactoryAndController.js
+++ web/modules/custom/bglobal_angular/components/programs/programFactoryAndController.js
@@ -8,6 +8,8 @@ const removePoundFromKeys = (obj) => {
   });
   return newObj;
 }
+
+// PROGRAMS
 app.factory('programFactory', ['$http', ($http) => {
   const pf = {};
   pf.program = removePoundFromKeys(drupalSettings.program);
@@ -18,6 +20,53 @@ app.controller('ProgramController',  ['$scope', 'programFactory', ($scope, progr
   const pc = {};
   const pf = programFactory;
   $scope.program = pf.program;
-  console.log($scope.program);
   return pc;
 }]);
+
+// COURSES
+app.factory('coursesFactory', ['$http', ($http) => {
+  const cf = {};
+  const program = removePoundFromKeys(drupalSettings.program);
+  cf.urlBuilder = (instance) => `https://${instance}-berkeley-web-services.pantheon.berkeley.edu/courses/object/`;
+  cf.getCourses = (cert, instance = 'live') => {
+    let courseJsonUrl = `${cf.urlBuilder(instance)}${program.certificates[cert].courses}?_format=json`;
+    return $http
+      .get(courseJsonUrl)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((response) => {
+        if (cf.errorCount <= 1) return cf.getServerData(type, 'test');
+        return [];
+      });
+  };
+  return cf;
+}]);
+
+app.controller('CoursesController',  ['$scope', 'coursesFactory', ($scope, coursesFactory) => {
+  const cc = {};
+  const cf = coursesFactory;
+  cf.getCourses($scope.cert)
+    .then((courses) => {
+      $scope.courses = courses;
+    })
+    .catch((err) => {
+      console.error('The error in CoursesController is:');
+      console.error(err);
+    });
+  return cc;
+}]);
+
+app.directive('certificateCourses', () => {
+  return {
+    restrict: 'A',
+    scope: {
+      'cert': '@',
+    },
+    template: `<ul><li data-ng-repeat="course in courses"><a href="https://extension.berkeley.edu/search/publicCourseSearchDetails.do?method=load&courseId={{course.objectId}}&showSections=false">{{ course.name }}</a> | {{ course.number }}</p></li>
+      <li data-ng-if="cert == 0"><a href="https://voices.berkeley.edu/international/preparing-students-next-step-their-careers" tab="_blank" rel="noreferrer noopener">Career Development Leadership Skills</a> | BUS ADM 205 </li>
+      <li data-ng-if="cert == 1"><a href="https://voices.berkeley.edu/international/preparing-students-next-step-their-careers" tab="_blank" rel="noreferrer noopener">Career Development Fundamentals</a> | BUS ADM 2054 </li>
+    </ul>`,
+    controller: 'CoursesController',
+  };
+});
